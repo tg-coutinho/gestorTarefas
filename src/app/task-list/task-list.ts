2ca6873309bb2e86bdac4fe196716dec00e5d042
tg-coutinho <tcoutinho908@gmail.com>
2025-07-11 17:58:39 -0300

    criando html dos componentes

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TaskFilter } from "../task-filter/task-filter";
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [TaskFilter],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css'
 })
